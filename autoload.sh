482715078a8ada75ae33439383175887ff42e30e
Oopsno <yufeiliu94@gmail.com>
2015-10-10 17:27:50 +0800

    fix pkg

--- autoload.sh
+++ autoload.sh
@@ -1,3 +1,12 @@
 vendor-setup () {
     $(vendor-setup-gen $@)
 }
+
+vendor-pkg-path () {
+    if [ $# -eq 0 ]
+    then
+        vendor-pkg-path-gen
+    else
+        $(vendor-pkg-path-gen $@)
+    fi
+}
